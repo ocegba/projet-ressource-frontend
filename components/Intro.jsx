a020c19f8387dbf751fc06aed373fb2bcb8488e6
ocegba <oceane.gba@gmail.com>
2022-02-21 15:51:14 +0100

    pt3

---
+++ components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Intro.module.css'
+
+const Intro = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.card}>first</div>
+        <div className={styles.card}>two</div>
+    </div>
+  )
+}
+
+export default Intro
