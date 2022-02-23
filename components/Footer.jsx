a79a61550118f0c2fa86e91c85f3ee8b407056f6
ocegba <oceane.gba@gmail.com>
2022-02-23 09:25:54 +0100

    modif pt3

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,9 +6,9 @@ import logoentreprise from "../public/img/logoentreprise.png"
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.footercontainer}>
       <Image className={styles.logo} src={logo} alt="loading..." width={260} height={260}/>
-      <ul className={styles.textabout}>
+      <ul className={styles.footercontenttext}>
         <p>
         RESSOURCES RELATIONNELLES
         </p>
@@ -19,12 +19,17 @@ const Footer = () => {
         </p>
         <a href="mailto:contact@resourcesrelationnelles.fr">contact@resourcesrelationnelles.fr</a>
       </ul>
-      <ul className={styles.pdfabout}>
+      <ul className={styles.footercontentpdf}>
         <a href="mentionslegales.pdf" download="mentionslegales.PDF">Mentions légales</a>
         <a href="PolitiqueDeFonctionnalité.pdf" download="PolitiqueDeFonctionnalité.PDF">Politique de confidentialité</a>
         <a href="Conditions.pdf" download="Conditions.PDF">Conditions générales d'utilisation Resources Relationnelles</a>
       </ul>
       <Image className={styles.logoentreprise} src={logoentreprise} alt="loading..." width={260} height={260}/>
+      {/* <div class="footer-bottom">
+        <p>Copyright &copy;2022 <a href="#">ReSources Relationnelles</a>  </p>
+      </div>  */}
+      
+      
     </div>
   )
 }
--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from '../styles/Intro.module.css'
+import gifsegway from '../public/img/Segway Scooter.gif'
+import Image from 'next/image'
 
 const Intro = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.card}>first</div>
-        <div className={styles.card}>two</div>
+        <div className={styles.band}>
+          <Image className={styles.gifsegway} src={gifsegway} alt="loading..."/>
+        
+        
+        </div>
+        <div className={styles.band}>
+          two
+        </div>
     </div>
   )
 }
