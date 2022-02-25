f3692cf3483ac664a0cdcad24bc51f1e1d2a6d16
ocegba <oceane.gba@gmail.com>
2022-02-25 12:06:32 +0100

    Parie en savoir plus faite et debut formulaire

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import styles from '../styles/Intro.module.css'
-import gifsegway from '../public/img/Segway Scooter.gif'
 import Image from 'next/image'
+import logo from "../public/img/logo.gif" 
+import Link from 'next/link'
 
 const Intro = () => {
   return (
-    <div className={styles.container}>
-        <div className={styles.band}>
-          <Image className={styles.gifsegway} src={gifsegway} alt="loading..."/>
-        
-        
+    <div className={styles.IntroContainer}>
+      <div className={styles.IntroPres}>
+        <div className={styles.IntroImage}>
+            <div className={styles.IntroContent}>
+              <div className={styles.IntroItems}>
+                  <h1>(Re)Sources Relationnelles</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit temporibus doloribus quo dolorem, rem omnis, facere autem, eius quidem necessitatibus tenetur ex tempore. Voluptatem, dignissimos dolorem. Nemo blanditiis voluptatem fugiat?</p>
+              </div>
+            </div>
         </div>
-        <div className={styles.band}>
-          two
+      </div>
+      <div className={styles.searchHomeRes}>
+        <div className={styles.card}>
+          <Image className={styles.logo} src={logo} alt="loading..." width={230} height={230} />
+          <h2 className={styles.subTitle}>Chercher une ressource</h2>
+          <p>Découvrez des activités qui sont proposées par notre communauté<br/><br/>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis qui aliquid nam aspernatur ullam recusandae error hic autem odit. Non qui id sequi est voluptatem! Voluptatibus, at rerum! Fugit, officiis.</p>
+          <li className={styles.morepdf}>
+            <Link href="/more">En savoir plus</Link>
+          </li>
         </div>
+        <div className={styles.card}>
+          <h2 className={styles.subTitle}>Consulter les ressources de la communautée</h2>
+
+        </div>
+      </div>
     </div>
-  )
+      )
 }
 
-export default Intro
+      export default Intro
