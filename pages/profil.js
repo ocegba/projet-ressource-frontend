8b4000eeb03e16be7e303c14e0a9d6ff154dff05
ocegba <oceane.gba@gmail.com>
2022-03-16 22:15:02 +0100

    profil pt1

--- pages/profil.js
+++ pages/profil.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import SideBar from '../components/SideBar'
 import styles from '../styles/about.module.css'
+import Head from 'next/head'
 
 const about = () => {
-  return (
+    return (
         <div className={styles.container}>
-            <p>Bienvenue sur votre profil.</p>
+            <Head>
+                <title>(Re)Sources relationnelles</title>
+                <meta name="description" content="(Re)Sources relationnelles, un guide pour vos relations" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <SideBar />
+            <div class="w-full h-full p-4 m-8 overflow-y-auto">
+                <div class="flex items-center justify-center p-40 border-4 border-dotted">
+                    
+                </div>
+            </div>
         </div>
+
     )
 }
 
