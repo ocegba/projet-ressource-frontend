5ce10681be55fc4f02a2b43abb39622732ca131a
ocegba <82329248+ocegba@users.noreply.github.com>
2022-03-20 21:31:39 +0100

    Navbar responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,63 +1,51 @@
 import React, { useState } from 'react'
-import styles from "../styles/Navbar.module.css"
 import Image from 'next/image'
 import logo from "../public/img/logo.gif"
-import Link from 'next/link'
 
 const Navbar = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className={styles.container}>
-      <a href="/" alt="loading..."><Image className={styles.logo} src={logo} width={80} height={80} /></a>
-      <ul className={styles.list}>
-        <li className={styles.listItem}>
-          <Link href="/createRessource">Créer une ressource</Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/searchRessource">Rechercher une ressource</Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/about">À propos</Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/signin">Se connecter</Link>
-        </li>
-      </ul>
-      <ul className={styles.list}>
-        <li className={styles.listItemSign}>
-          <Link href="/signup">S'inscrire</Link>
-        </li>
-        <li className={styles.listItemHelp}>
-          <Link href="/help">Aide</Link>
-        </li>
-      </ul>
-      <div className={styles.hamburger} onClick={() => setOpen(!open)} >
-        <div className={styles.line} />
-        <div className={styles.line} />
-        <div className={styles.line} />
+    <nav class="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div class="lg:container flex flex-wrap justify-between items-center mx-auto">
+        <a href="#" class="flex items-center">
+          <a href="/" alt="loading..."><Image class="rounded-2xl" src={logo} width={80} height={80} /></a>
+        </a>
+        <div class="flex md:order-2">
+          <li>
+            <a href="/signup" class="text-white bg-blue-300 hover:bg-slate-400 focus:ring-3 focus:ring-slate-400 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >S'inscrire</a>
+          </li>
+          <li>
+            <a href="/help" class="text-white bg-gray-500 hover:bg-gray-700 focus:ring-3 focus:ring-gray-700 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Aide</a>
+          </li>
+
+          <button onClick={() => setOpen(!open)} data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-xl text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
+            <span class="sr-only">Ouvrir le menu principal</span>
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+        </div>
+        <div class="md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-4">
+          {!open ? (
+            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-xl md:font-medium">
+              <li>
+                <a href="/createRessource" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-300 md:p-0 md:dark:hover:text-black dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Créer une ressource</a>
+              </li>
+              <li>
+                <a href="/searchRessource" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-300 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Rechercher une ressource</a>
+              </li>
+              <li>
+                <a href="/about" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-300 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">À propos</a>
+              </li>
+              <li>
+                <a href="/signin" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-300 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Se connecter</a>
+              </li>
+            </ul>
+            ) : false }
+        </div>
       </div>
-      <ul onClick={() => setOpen(false)} className={styles.menu} style={{ right: open ? "0px" : "-50vw" }}>
-        <li className={styles.menuItem}>
-          <Link href="/createRessource">Créer une ressource</Link>
-        </li>
-        <li className={styles.menuItem}>
-          <Link href="/searchRessource">Rechercher une ressource</Link>
-        </li>
-        <li className={styles.menuItem}>
-          <Link href="/about">À propos</Link>
-        </li>
-        <li className={styles.menuItem}>
-          <Link href="/signin">Se connecter</Link>
-        </li>
-        <li className={styles.menuItemSign}>
-          <Link href="/signup">S'inscrire</Link>
-        </li>
-        <li className={styles.menuItemHelp}>
-          <Link href="/help">Aide</Link>
-        </li>
-      </ul>
-    </div>
+    </nav>
+
   )
 }
 
