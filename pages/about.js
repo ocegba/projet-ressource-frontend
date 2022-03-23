9377b3dcbbb8b4b0169ba2a1a502fd6c2161457b
ocegba <82329248+ocegba@users.noreply.github.com>
2022-03-23 08:50:27 +0100

    styles About

--- pages/about.js
+++ pages/about.js
@@ -7,14 +7,14 @@ import logoministere from "../public/img/Ministère_des_Solidarités_et_de_la_Sa
 
 const about = () => {
   return (
-        <div className={styles.container}>
-            <p><b>(Re)Sources relationnelles</b> a été réalisé par l’entreprise <b>4APPS</b> proposé par le <b>ministère des Solidarités et de la Santé.</b></p>
+        <div class="h-fit w-fit ml-2.5">
+            <p class="w-auto text-justify text-4xl"><b>(Re)Sources relationnelles</b> a été réalisé par l’entreprise <b>4APPS</b> proposé par le <b>ministère des Solidarités et de la Santé.</b></p>
             <div>
-                <Image className={styles.logo} src={logo} alt="loading..." width={260} height={260} />
-                <Image className={styles.logo} src={logoentreprise} alt="loading..." width={260} height={260} />
-                <Image className={styles.logo} src={logoministere} alt="loading..." width={260} height={260} />
+                <Image class="rounded-3xl" src={logo} alt="loading..." width={260} height={260} />
+                <Image class="rounded-3xl" src={logoentreprise} alt="loading..." width={260} height={260} />
+                <Image class="rounded-3xl" src={logoministere} alt="loading..." width={260} height={260} />
             </div>
-            <p>Cette application permet de renforcer les liens entre les citoyens à travers diverses ressources.</p>
+            <p class="w-auto text-justify text-4xl">Cette application permet de renforcer les liens entre les citoyens à travers diverses ressources.</p>
         </div>
     )
 }
